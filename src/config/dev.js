3e20a53785677faa92f71e9ad1a14ede7442a61c
hakanErgin <hakan.ergin@gmail.com>
2020-05-14 18:03:29 +0200

    initial commit

--- src/config/dev.js
+++ src/config/dev.js
@@ -2,5 +2,5 @@ export const config = {
   secrets: {
     jwt: 'learneverything'
   },
-  dbUrl: 'mongodb://localhost:27017/api-design'
+  dbUrl: 'mongodb+srv://hakan:ZLZI1atQS7oh9Hl0@cluster0-ivbc4.mongodb.net/test'
 }
